450de9fddc2774328ee6053c01e4d76e91b80b88
RobinsonSdG <robsdg13@gmail.com>
2023-10-27 08:54:50 +0200

    update slide almost working, some bugs remaining

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use std::{fs::{File, self}, io::{BufReader, BufWriter}, ffi::OsString};
+use std::{fs::{File, self, rename}, io::{BufReader, BufWriter, Read}, ffi::OsString};
 
 use xml::reader::{EventReader, XmlEvent};
 
 use rand::Rng;
 use walkdir::WalkDir;
 
+use std::path::Path;
+
 use reqwest::blocking::Response;
 use minidom::Element;
+use zip::{ZipWriter, write::FileOptions, CompressionMethod};
 use std::io::Write;
 use regex::Regex;
 use std::io::BufRead;
 use jumprankingsapi::models::ranking_model::{Rank, Ranking, Rankings};
 
 fn main() -> std::io::Result<()> {
-    let src = "SlideTemplateStp";
+    let mut src = "SlideTemplateStp";
     let year = 2023;
-    let week = 42;
+    let week = 47;
     
     let ranking = get_ranking(year, week);
     let previous_ranking = get_ranking(year, week-1);
 
-    // for r in ranking.ranking.iter().rev() {
-    //     println!("{:?}", r.name);
-    // }
-    let mut counter_slide = 7;
+    copy_directory(Path::new(src), Path::new("Tmp"))?;
+    src = "Tmp";
+
+    let mut counter_slide = 8;
     // COLOR PAGES
     let mut nb = 1;
     for image_src in ranking.color_pages {
@@ -35,10 +38,10 @@ fn main() -> std::io::Result<()> {
                 let img = image::load_from_memory(&img_bytes).unwrap();
                 img.save(format!("{}/ppt/media/colorPage{}.png", src, nb)).unwrap();
                 if img.height() > img.width() {
-                    add_color_slide(src, 1, nb, counter_slide)?;
+                    add_color_slide(src, 2, nb, counter_slide)?;
                     counter_slide += 1;
                 } else {
-                    add_color_slide(src, 2, nb, counter_slide)?;
+                    add_color_slide(src, 3, nb, counter_slide)?;
                     counter_slide += 1;
                 }
                 nb+=1;
@@ -69,6 +72,36 @@ fn main() -> std::io::Result<()> {
     }
     
     // add_slide(src, 3)?;
+
+    create_pptx(src)?;
+    Ok(())
+}
+
+fn create_pptx(src: &str) -> std::io::Result<()> {
+    let archive_name = "archive.zip";
+
+    let archive_file = File::create(archive_name)?;
+    let mut archive = ZipWriter::new(archive_file);
+
+    for entry in WalkDir::new(src).into_iter().filter_map(|e| e.ok()) {
+        if !entry.file_type().is_file() {
+            continue; // Ignore directories
+        }
+
+        let file_name_in_zip = entry.path().strip_prefix(src).unwrap().to_string_lossy().into_owned();
+        let file_content = File::open(entry.path())?;
+
+        let options = FileOptions::default()
+            .compression_method(CompressionMethod::Stored); // Preserve file permissions
+
+        archive.start_file(file_name_in_zip, options)?;
+        let mut buffer = Vec::new();
+        file_content.take(u64::MAX).read_to_end(&mut buffer)?;
+
+        archive.write_all(&buffer)?;
+    }
+
+    rename(archive_name, "presentation.pptx")?;
     Ok(())
 }
 
@@ -156,7 +189,7 @@ fn add_slide(src: &str, rank: &Rank, rank_position: i8, previous_rank_position :
     // fs::copy(format!("{}/ppt/slides/slide{}.xml", src, slide_num-1), format!("{}/ppt/slides/slide{}.xml", src, slide_num))?;
     // 2. modifier le slideX.xml:
 
-    update_slide(src, slide_num, rank_position, variation, current_slide, &rank.name)?;
+    update_slide(src, slide_num, rank_position, variation, current_slide, &rank.name, rank.chapter)?;
     /* 
     - La couleur du texte de variance: <a:srgbClr val="FF0000" />
     - La valeur de la variance: <a:t>-1</a:t>
@@ -236,58 +269,153 @@ fn update_presentation_xml(src: &str, r_id: i32) -> std::io::Result<()> {
 }
 
 //Possibilité de créer le nouveau fichier en même temps
-fn update_slide(src: &str, slide_num: i8, rank: i8, rank_changement: Option<i8>, counter_slide: i8, name: &String) -> std::io::Result<()> {
+fn update_slide(src: &str, slide_num: i8, rank: i8, rank_changement: Option<i8>, counter_slide: i8, name: &String, chapter: i16) -> std::io::Result<()> {
+    let input_filename = format!("{}/ppt/slides/slide{}.xml", src, slide_num);
+    let output_filename = format!("{}/ppt/slides/slide{}.xml", src, counter_slide);
 
-    let search_pattern = r"<a:t>-?\d+</a:t>";
+    let mut input_file = File::open(input_filename)?;
+    let mut input_contents = String::new();
+    input_file.read_to_string(&mut input_contents)?;
 
-    let mut replace_with = String::new();
+    let re1 = Regex::new(r#"<a:t>([+-]?\d+)</a:t>"#).unwrap();
+    let re2 = Regex::new(r#"<a:t>([a-zA-Z\s]+)</a:t>"#).unwrap();
+    let re3 = Regex::new(r#"<a:t>\(#([0-9]+)\)</a:t>"#).unwrap();
+    let re4 = Regex::new(r#"<a:srgbClr val="BB1A00"/></a:solidFill></a:ln><a:solidFill><a:srgbClr val="FFB10B"/></a:solidFill>"#).unwrap();
+
+    let mut replacement = String::new();
+    let mut color = String::new();
+    let mut outline = String::new();
+    let mut color_change = false;
     match rank_changement {
         Some(r) => {
             println!("{} rank_changement: {}", name, r);
             if r > 0 {
-                replace_with = format!("<a:t>+{}</a:t>", r); //r
+                replacement = format!("<a:t>+{}</a:t>", r); //r
+                color = "2EF729".to_string();
+                outline = "DF5656".to_string();
+                color_change = true;
             } else if r < 0 {
-                replace_with = format!("<a:t>-{}</a:t>", r); //r
+                replacement = format!("<a:t>{}</a:t>", r); //r
+                color = "DF5656".to_string();
+                outline = "BB1A00".to_string();
+                color_change = true;
             } else if r == 0 {
-                replace_with = "<a:t>=</a:t>".to_string(); //rank
+                replacement = "<a:t>=</a:t>".to_string(); //rank
             }
         },
-        None => replace_with = r"<a:t>new</a:t>".to_string(),
+        None => replacement = r"<a:t>in</a:t>".to_string(),
     };
+
+    let mut modified_contents: String = "".to_string();
+    if let Some(caps) = re1.captures(&input_contents) {
+        let first_match = caps.get(0).unwrap().as_str();
+
+        modified_contents = input_contents.replacen(first_match, &replacement, 1);
+
+        println!("Remplacement de la première occurrence terminé. Le résultat a été enregistré dans '{}'", &output_filename);
+    } else {
+        println!("Aucune première correspondance trouvée dans le fichier.");
+    }
+    if let Some(caps) = re2.captures(&input_contents) {
+        let first_match = caps.get(0).unwrap().as_str();
+
+        modified_contents = modified_contents.replacen(first_match, format!("<a:t>{}</a:t>", &name).as_str(), 1);
+
+        println!("Remplacement de la deuxième occurrence terminé. Le résultat a été enregistré dans '{}'", &output_filename);
+    } else {
+        println!("Aucune deuxième correspondance trouvée dans le fichier.");
+    }
+    if let Some(caps) = re3.captures(&input_contents) {
+        let first_match = caps.get(0).unwrap().as_str();
+
+        modified_contents = modified_contents.replacen(first_match, format!("<a:t>(#{})</a:t>", &chapter).as_str(), 1);
+
+        println!("Remplacement de la troisième occurrence terminé. Le résultat a été enregistré dans '{}'", &output_filename);
+    } else {
+        println!("Aucune troisième correspondance trouvée dans le fichier.");
+    }
+    if color_change {
+        if let Some(caps) = re4.captures(&input_contents){
+            let first_match = caps.get(1).unwrap().as_str();
     
-    let input_file_path = format!("{}/ppt/slides/slide{}.xml", src, slide_num);
-    let output_file_path = format!("{}/ppt/slides/slide{}.xml", src, counter_slide);
+            modified_contents = modified_contents.replacen(first_match, format!("<a:srgbClr val=\"{}\"/></a:solidFill></a:ln><a:solidFill><a:srgbClr val=\"{}\"/></a:solidFill>", &outline, &color).as_str(), 1);
+    
+            println!("Remplacement de la quatrième occurrence terminé. Le résultat a été enregistré dans '{}'", &output_filename);
+        } else {
+            println!("Aucune quatrième correspondance trouvée dans le fichier.");
+        }
+    }
+     
 
-    fs::copy(input_file_path, &output_file_path)?;
+    let mut output_file = File::create(&output_filename)?;
+    output_file.write_all(modified_contents.as_bytes())?;
+    // let search_pattern = r"<a:t>([+-]?\d+)</a:t>";
 
-    let output_file = File::open(&output_file_path)?;
-
-    // // Ouvrez le fichier de sortie en écriture
-    // let output_file = match File::create(output_file_path) {
-    //     Ok(file) => file,
-    //     Err(err) => {
-    //         panic!("Erreur lors de la création du fichier de sortie : {}", err);
-    //     }
+    // let mut replace_with = String::new();
+    // match rank_changement {
+    //     Some(r) => {
+    //         println!("{} rank_changement: {}", name, r);
+    //         if r > 0 {
+    //             replace_with = format!("<a:t>+{}</a:t>", r); //r
+    //         } else if r < 0 {
+    //             replace_with = format!("<a:t>-{}</a:t>", r); //r
+    //         } else if r == 0 {
+    //             replace_with = "<a:t>=</a:t>".to_string(); //rank
+    //         }
+    //     },
+    //     None => replace_with = r"<a:t>new</a:t>".to_string(),
     // };
+    
+    // let input_file_path = format!("{}/ppt/slides/slide{}.xml", src, slide_num);
+    // let output_file_path = format!("{}/ppt/slides/slide{}.xml", src, counter_slide);
 
-    // Créez un lecteur pour le fichier d'entrée
-    let reader = BufReader::new(output_file);
+    // let mut input_file = File::open(&input_file_path)?;
 
-    // Créez un écrivain pour le fichier de sortie
+    // let mut output_file = File::create(output_file_path).expect("Impossible de créer le fichier destination");
+    // let mut buffer = Vec::new();
+    // // Lisez les données du fichier source dans le tampon
+    // input_file.read_to_end(&mut buffer).expect("Impossible de lire le fichier source");
+    // // Écrivez le contenu du tampon dans le fichier destination
+    // // output_file.write_all(&buffer).expect("Impossible d'écrire dans le fichier destination");
+
+    // // fs::copy(&input_file_path, &output_file_path)?;
+
+    
+    
+    // // let output_file = File::(&output_file_path)?;
+
+    // // // Ouvrez le fichier de sortie en écriture
+    // // let output_file = match File::create(output_file_path) {
+    // //     Ok(file) => file,
+    // //     Err(err) => {
+    // //         panic!("Erreur lors de la création du fichier de sortie : {}", err);
+    // //     }
+    // // };
+
+    // // Créez un lecteur pour le fichier d'entrée
+    // let mut reader = BufReader::new(input_file);
+
+    // // Créez un écrivain pour le fichier de sortie
     // let mut writer = BufWriter::new(output_file);
 
-    // Créez une expression régulière pour la recherche
-    let regex = Regex::new(search_pattern).expect("Erreur lors de la création de l'expression régulière");
+    // // Créez une expression régulière pour la recherche
+    // let regex = Regex::new(search_pattern).expect("Erreur lors de la création de l'expression régulière");
+    // println!("ici");
+    // let mut text: String = "".to_string();
+    // reader.read_to_string(&mut text)?;
+    // let modified_line = regex.replace(&text, &replace_with);
+    // writer.write(modified_line.as_bytes())?;
+    // // for line in reader.lines() {
+    // //     println!("là");
+    // //     let line = line.expect("Erreur lors de la lecture de la ligne");
 
-    for line in reader.lines() {
-        let line = line.expect("Erreur lors de la lecture de la ligne");
+    // //     // Utilisez l'expression régulière pour rechercher et remplacer dans la ligne
+        
+    // //     println!("modified_line: {}", modified_line);
 
-        // Utilisez l'expression régulière pour rechercher et remplacer dans la ligne
-        let modified_line = regex.replace_all(&line, &replace_with);
-
-        // Écrivez le résultat modifié dans le fichier de sortie
-        // writeln!(writer, "{}", modified_line).expect("Erreur lors de l'écriture dans le fichier de sortie");
-    }
+    // //     // Écrivez le résultat modifié dans le fichier de sortie
+    // //     writeln!(writer, "{}", modified_line).expect("Erreur lors de l'écriture dans le fichier de sortie");
+    // // }
     Ok(())
 }
 
@@ -379,4 +507,19 @@ fn update_rels(src: String, slide_num: i8) -> std::io::Result<i32> {
     let mut file = File::create(&src)?;
     file.write_all(&buf)?;
     Ok(id)
+}
+
+fn copy_directory(src: &Path, dst: &Path) -> std::io::Result<()> {
+    if src.is_dir() {
+        fs::create_dir_all(dst)?;
+        for entry in fs::read_dir(src)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+            let new_dst = dst.join(entry_path.file_name().unwrap());
+            copy_directory(&entry_path, &new_dst)?;
+        }
+    } else {
+        fs::copy(src, dst)?;
+    }
+    Ok(())
 }
